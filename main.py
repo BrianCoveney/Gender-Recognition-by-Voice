2527d792b3c0e0bb0240bd57f5707f982ecb0a05
Brian Coveney <coveneybrian@gmail.com>
2017-11-20 11:35:03 +0000

    Add data cleansing

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from sklearn import naive_bayes
 from sklearn.grid_search import GridSearchCV
 from sklearn import model_selection
 from sklearn import linear_model
+import sklearn.metrics as metrics
 from sklearn.datasets import load_svmlight_files
 import numpy as np
 import seaborn as sns
@@ -18,11 +19,11 @@ def runClassifiersCV(data, target):
     print("\nThe following are the initial accuracies using CV 10")
     dTree = tree.DecisionTreeClassifier()
     scores = model_selection.cross_val_score(dTree, data, target, cv=10)
-    print("Tree : ", scores.mean())
+    print("Tree        : ", scores.mean())
 
     rbfSvm = SVC()
     scores = model_selection.cross_val_score(rbfSvm, data, target, cv=10)
-    print("SVM : ", scores.mean())
+    print("SVM         : ", scores.mean())
 
     nearestN = KNeighborsClassifier()
     scores = model_selection.cross_val_score(nearestN, data, target, cv=10)
@@ -30,7 +31,7 @@ def runClassifiersCV(data, target):
 
     randomForest = RandomForestClassifier()
     scores = model_selection.cross_val_score(randomForest, data, target, cv=10)
-    print("RForest : ", scores.mean())
+    print("RForest     : ", scores.mean())
 
     nBayes = naive_bayes.GaussianNB()
     scores = model_selection.cross_val_score(nBayes, data, target, cv=10)
@@ -38,18 +39,18 @@ def runClassifiersCV(data, target):
 
     logR = linear_model.LogisticRegression()
     scores = model_selection.cross_val_score(logR, data, target, cv=10)
-    print("Log R : ", scores.mean())
+    print("Log R       : ", scores.mean())
 
 
 def runModelSelectionRandomForest(data, target):
     param_grid = [{'n_estimators': list(range(10, 400, 30)), 'criterion': ["gini", "entropy"],
                    "max_features": ["auto", "log2", "sqrt"]}]
 
-    clf = GridSearchCV(RandomForestClassifier(random_state=10), param_grid, cv=10)
+    clf = GridSearchCV(RandomForestClassifier(n_jobs=2, random_state=0), param_grid, cv=10)
 
     clf.fit(data, target)
 
-    print("\n Best parameters set found on development set:")
+    print("\n Best parameters set found on RandomForest development set:")
 
     print(clf.best_params_, "with a score of ", clf.best_score_)
 
@@ -63,7 +64,7 @@ def runModelSelectionKNN(data, target):
 
     clf.fit(data, target)
 
-    print("\n Best parameters set found on development set:")
+    print("\n Best parameters set found o KNN development set:")
 
     print(clf.best_params_, "with a score of ", clf.best_score_)
 
@@ -101,26 +102,84 @@ def performPreprocessing(gender):
     # Normalize the Data to standardize the features
     scalingObj = preprocessing.MinMaxScaler()
     gender[["meanfreq","sd","median","Q25","Q75","IQR","skew","kurt","sp.ent","sfm","mode","centroid","meanfun","minfun",
-            "maxfun","meandom","mindom","maxdom","dfrange","modindx"]] = scalingObj.fit_transform(gender[["meanfreq","sd",
-            "median","Q25","Q75","IQR","skew","kurt","sp.ent","sfm","mode","centroid","meanfun","minfun","maxfun","meandom",
-            "mindom","maxdom","dfrange","modindx"]])
+            "maxfun","meandom","mindom","maxdom","dfrange","modindx"]] = scalingObj.fit_transform(
+        gender[["meanfreq","sd","median","Q25","Q75","IQR","skew","kurt","sp.ent","sfm","mode","centroid","meanfun","minfun",
+                "maxfun","meandom","mindom","maxdom","dfrange","modindx"]])
 
     return gender
 
 
+#
+# Data Cleaning
+# Ref. https://en.wikipedia.org/wiki/Voice_frequency
+# The voiced speech of a typical adult:
+# male fundamental frequency from 85 to 180 Hz
+# female fundamental frequency from 165 to 255 Hz
+#
+def dataCleansing(df_train, df_test):
+
+    print("Scatter plot pre data cleansing \n"
+          "Please close window to continue")
+    sns.FacetGrid(df_test, hue="label", size=5) \
+        .map(plt.scatter, "meanfun", "meanfreq") \
+        .add_legend()
+    plt.show()
+
+    # Filtering ouliers from male category
+    male_funFreq_outlier_index = df_train[((df_train['meanfun'] < 0.085) | (df_train['meanfun'] > 0.180)) &
+                                              (df_train['label'] == 'male')].index
+    male_funFreq_outlier_index = list(male_funFreq_outlier_index)
+    df_train[((df_train['meanfun'] < 0.085) | (df_train['meanfun'] > 0.180)) & (
+        df_train['label'] == 'male')].shape
+
+    # Filtering ouliers from female category
+    female_funFreq_outlier_index = df_train[
+        ((df_train['meanfun'] < 0.165) | (df_train['meanfun'] > 0.255)) &
+        (df_train['label'] == 'female')].index
+    female_funFreq_outlier_index = list(female_funFreq_outlier_index)
+    df_train[((df_train['meanfun'] < 0.165) | (df_train['meanfun'] > 0.255)) & (
+        df_train['label'] == 'female')].shape
+
+    index_to_remove = male_funFreq_outlier_index + female_funFreq_outlier_index
+    print("Index to remove:",len(index_to_remove))  # prints 710
+
+    print("Data size", df_train.shape)  # prints (3168, 21)
+
+    df_train = df_train.drop(index_to_remove, axis=0)
+    print("Data size", df_train.shape)  # prints (2458, 21)
+
+    df_test = df_test.drop(index_to_remove, axis=0)
+    print("Data size", df_test.shape)  # prints (2458, 21)
+
+    print("Scatter plot post data cleansing \n"
+          "Please close window to continue")
+    sns.FacetGrid(df_test, hue="label", size=5) \
+        .map(plt.scatter, "meanfun", "meanfreq") \
+        .add_legend()
+    plt.show()
+
+
 def main():
     gender_train = pd.read_csv("res/train_voice.csv", delimiter=",")
-    feature_train = performPreprocessing(gender_train)
-
     gender_test = pd.read_csv("res/test_voice.csv", delimiter=",")
-    feature_test = performPreprocessing(gender_test)
 
     # There are 3168 labels split evenly between male and female. With no null values.
-    # print("Number of labels:", gender_train.shape[0])
-    # print("Number of males:", gender_train[gender_train["label"] == "male"].shape[0])
-    # print("Number of females:", gender_train[gender_train["label"] == "female"].shape[0])
-    # print(gender_train.isnull().sum())
-    # print(gender_train.head())
+    print("The following is a look at the dataset:")
+    print("Number of labels: ", gender_train.shape[0])
+    print("Number of males:  ", gender_train[gender_train["label"] == "male"].shape[0])
+    print("Number of females:", gender_train[gender_train["label"] == "female"].shape[0])
+    if gender_train.isnull() is True:
+        print('There are missing values')
+    else:
+        print('No missing values')
+
+    dataCleansing(gender_train, gender_test)
+
+
+    feature_train = performPreprocessing(gender_train)
+    feature_test = performPreprocessing(gender_test)
+
+
 
     # Split the training dataset into features and classes. A 1D class array and 2D features array.
 
@@ -133,22 +192,26 @@ def main():
 
     runClassifiersCV(feature_train, label_train)
 
-    # print("FEATURE", feature_train.head())
-    # print("LABEL", label_train.head())
 
-    bestModel = runModelSelectionRandomForest(feature_train, label_train)
+
+    # Best parameters set found on RandomForest development set:
+    # {'criterion': 'gini', 'max_features': 'auto', 'n_estimators': 70} with a score of  0.9681186868686869
+    # bestModel = runModelSelectionRandomForest(feature_train, label_train)
+
+    # Best parameters set found o KNN development set:
+    # {'n_neighbors': 3, 'p': 2, 'weights': 'uniform'} with a score of  0.95864898989899
     # bestModel = runModelSelectionKNN(feature_train, label_train)
 
+    # Best parameters set found on SVC development set:
     # {'C': 1, 'gamma': 0.001, 'kernel': 'linear'} with a score of  0.9696969696969697
     # bestModel = runModelSelectionSVC(feature_train, label_train)
-
-
-    results = bestModel.predict(feature_test)
-
-    resultSeries = pd.Series(data=results, name='label', dtype='int64')
-
-    df = pd.DataFrame({"PassengerId": genderSeries, "label": resultSeries})
-    df.to_csv("res/kaggle_gridSearch.csv", index=False, header=True)
+    #
+    # results = bestModel.predict(feature_test)
+    #
+    # resultSeries = pd.Series(data=results, name='label', dtype='int64')
+    #
+    # df = pd.DataFrame({"PassengerId": genderSeries, "label": resultSeries})
+    # df.to_csv("res/kaggle_gridSearch.csv", index=False, header=True)
 
 if __name__ == '__main__':
     main()
